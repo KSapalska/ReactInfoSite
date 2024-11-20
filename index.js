be1e14c05a03f86076ff236fec66dcb7dcdc33e0
SC-80414 <SC-80414@MA-L4B5R48JRJCN>
2024-11-20 14:41:49 +0100

    JSX

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //This is an declarative way to program
- ReactDOM.render( <h1 className="header" >Hello, Ract!</h1>,     document.getElementById("root"))  
+ //ReactDOM.render( <h1 className="header" >Hello, Ract!</h1>,     document.getElementById("root"))  
 
 
 //This is an imperative way to program
@@ -8,3 +8,21 @@
 h1.textContent = "This is an imperative way to program";
 h1.className = "header";
 document.getElementById("root").append(h1); */
+
+const navbar = (
+    <nav>
+        <h1> Brand name</h1>
+        <ul>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+        </ul>
+    </nav>
+    
+)
+
+ReactDOM.render(
+    navbar,
+    document.getElementById("root")
+    
+)
