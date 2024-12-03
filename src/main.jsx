1e17eb3eaf2f7a84672a6c4c655473878c301d8d
SC-80414 <SC-80414@MA-L4B5R48JRJCN>
2024-12-03 11:01:18 +0100

    Parent/Child Components

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,48 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
-//import { Fragment } from "react";
+import { createRoot } from "react-dom/client";
 
 import "./index.css";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<Page />);
+
+function Header() {
+  return (
+    <header>
+      <img src="src/assets/react-logo.png" alt="React logo" />
+    </header>
+  );
+}
+
+function MainContent() {
+  return (
+    <main>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thausands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
+}
+
+function Footer() {
+  <footer>
+    <small>© 2024 Sapalska development. All rights reserved.</small>
+  </footer>;
+}
 
 function Page() {
   return (
-    //<Fragment>
     <>
-      <header>
-        <img src="src/assets/react-logo.png" alt="React logo" />
-      </header>
-
-      <main>
-        <h1>Fun facts about React</h1>
-        <ul>
-          <li>Was first released in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 100K stars on GitHub</li>
-          <li>Is maintained by Meta</li>
-          <li>Powers thausands of enterprise apps, including mobile apps</li>
-        </ul>
-      </main>
-      
-      <footer>
-        <small>© 2024 Sapalska development. All rights reserved.</small>
-     </footer>
-        </>
-
-    //</Fragment>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
   );
 }
+
+root.render(<Page />);
+
