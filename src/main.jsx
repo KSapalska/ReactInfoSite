8b12759f95255a803ce50acfaf5cec2c21b505a4
SC-80414 <SC-80414@MA-L4B5R48JRJCN>
2024-12-03 09:23:08 +0100

    Custom Components

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,32 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import './index.css'
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <main>
-    <img src="src/assets/react-logo.png" alt="React logo"/>
-    <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Meta</li>
-            <li>Powers thausands of enterprise apps, including mobile apps</li>
-        </ul>
-    
+    <TemporaryName />
+);
+
+function TemporaryName() {
+  return (
+    <main>
+      <img src="src/assets/react-logo.png" alt="React logo" />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thausands of enterprise apps, including mobile apps</li>
+      </ul>
     </main>
-  
-) 
+  );
+}
+
+
+
+
+
 
 /*import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
@@ -53,6 +61,5 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <h1>Hello from React!</h1>
 ) */
-
 
 //ReactDOM.createRoot(document.getElementById("root")).render(navbar);
