3554d132a22994c95d4157cf1c54bb97f6101992
SC-80414 <SC-80414@MA-L4B5R48JRJCN>
2024-12-03 09:47:56 +0100

    Custom Components challange 2

--- src/main.jsx
+++ src/main.jsx
@@ -3,63 +3,30 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <TemporaryName />
-);
+root.render(<Page />);
 
-function TemporaryName() {
+function Page() {
   return (
-    <main>
-      <img src="src/assets/react-logo.png" alt="React logo" />
-      <h1>Fun facts about React</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on GitHub</li>
-        <li>Is maintained by Meta</li>
-        <li>Powers thausands of enterprise apps, including mobile apps</li>
-      </ul>
-    </main>
+    <div>
+      <header>
+        <img src="src/assets/react-logo.png" alt="React logo" />
+      </header>
+
+      <main>
+        <h1>Fun facts about React</h1>
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100K stars on GitHub</li>
+          <li>Is maintained by Meta</li>
+          <li>Powers thausands of enterprise apps, including mobile apps</li>
+        </ul>
+      </main>
+      
+      <footer>
+        <small>© 2024 Sapalska development. All rights reserved.</small>
+     </footer>
+
+    </div>
   );
 }
-
-
-
-
-
-
-/*import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'import
- import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-) */
-
-/* 
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-
-  const navbar = (
-    <nav>
-        <h1>Brand name</h1>
-        <ul>
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-    </nav>
-);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-
-root.render(
-    <h1>Hello from React!</h1>
-) */
-
-//ReactDOM.createRoot(document.getElementById("root")).render(navbar);
